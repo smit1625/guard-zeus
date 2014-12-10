82c22a3c5cff78d03097c8f9eefa73e91a63cbe7
Cezary Baginski <cezary@chronomantic.net>
2014-12-10 23:36:53 +0100

    temporarily disable warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
 
   config.disable_monkey_patching!
 
-  config.warnings = true
+  # config.warnings = true
 
   if config.files_to_run.one?
     config.default_formatter = 'doc'
