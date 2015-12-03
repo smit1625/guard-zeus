edbcac34beaee87e0fb69df9c19dad55fb92b4e1
MacKinley Smith <smit1625@msu.edu>
2015-12-03 15:39:51 -0700

    Reads from debug logs with awk and waits for zeus to be ready

--- lib/guard/zeus/runner.rb
+++ lib/guard/zeus/runner.rb
@@ -8,11 +8,12 @@ require 'guard/compat/plugin'
 module Guard
   class Zeus < Plugin
     class Runner
+      MAX_WAIT_COUNT = 10
       attr_reader :options
 
       def initialize(options = {})
         @zeus_pid = nil
-        @options = { run_all: true, logfile: nil }.merge(options)
+        @options = { run_all: true, logfile: nil, timeout: 30 }.merge(options)
         Compat::UI.info 'Guard::Zeus Initialized'
       end
 
@@ -29,6 +30,7 @@ module Guard
 
           # if it's active, use it
           if can_connect_to_socket?
+            @reusing_socket = true
             Compat::UI.info 'Guard::Zeus is re-using an existing .zeus.sock'
             return
           end
@@ -36,8 +38,12 @@ module Guard
           # just delete it
           delete_sockfile
         end
+ 
+        # check for an existing logfile
+        delete_logfile if !@reusing_socket && File.exist?(zeus_logfile)
 
         spawn_zeus zeus_serve_command, zeus_serve_options
+        wait_for_zeus_to_be_ready
       end
 
       def run(paths)
@@ -70,6 +76,11 @@ module Guard
       def delete_sockfile
         Compat::UI.info 'Guard::Zeus is deleting an unusable .zeus.sock'
         File.delete(sockfile)
+      end
+
+      def delete_sockfile
+        Compat::UI.info 'Guard::Zeus is deleting an existing logfile'
+        File.delete(zeus_logfile)
       end
 
       def rspec?
@@ -143,8 +154,10 @@ module Guard
       def zeus_serve_options
         opt_parts = []
         opt_parts << options[:cli] unless options[:cli].nil?
-        unless opt_parts.any? {|part| part =~ />/ }
-          opt_parts << " > #{zeus_logfile} 2>&1"
+        if ( log_parts = opt_parts.select{|part| part =~ /--log/ } ).any?
+          @zeus_logfile = log_parts[-1]
+        else
+          opt_parts << "--log #{zeus_logfile}"
         end
         opt_parts.join(' ')
       end
@@ -152,6 +165,43 @@ module Guard
       def zeus_logfile
         @zeus_logfile ||= options[:logfile] || File.join(Dir.pwd, 'log', 'zeus_output.log')
       end
+
+      def search_zeus_logfile(pattern)
+        `awk '#{pattern.to_s}{print $0}' #{zeus_logfile} | awk '{print $4}' | cut -d '/' -f 1`.strip.lines
+      end
+
+      def zeus_processes
+        return @zeus_processes if @zeus_processes
+        unbooted_processes = search_zeus_logfile(/unbooted/)
+        return [] if unbooted_processes.empty?
+        @zeus_processes = unbooted_processes
+      end
+      def zeus_ready_processes
+        search_zeus_logfile(/SReady/)
+      end
+
+      def zeus_booted?
+        zeus_processes.any?
+      end
+      def zeus_ready?
+        return true if @zeus_ready
+        return false unless zeus_booted?
+        return false if ( zeus_processes - zeus_ready_processes ).any?
+        @zeus_ready = true
+      end
+
+      def sleep_time; options[:timeout].to_f / MAX_WAIT_COUNT.to_f end
+      def wait_for_loop
+        count = 0
+        while !yield && count < MAX_WAIT_COUNT
+          wait_for_action
+          count += 1
+        end
+        !(count == MAX_WAIT_COUNT)
+      end
+      def wait_for_action; sleep sleep_time end
+      def wait_for_zeus_to_be_ready; wait_for_loop { zeus_ready? } end
+
     end
   end
 end
