45c7acfaa0ec59c64320dacbe8852762c55dc49a
MacKinley Smith <smit1625@msu.edu>
2015-12-04 15:17:13 -0700

    Another try

--- lib/guard/commander.rb
+++ lib/guard/commander.rb
@@ -7,19 +7,26 @@ require "guard/dsl_describer"
 
 require "guard/internals/state"
 
+OriginalGuardCommander = Guard::Commander
+
 module Guard
   module Commander
-    def self.included(base)
-      puts 'Test 1'
-      base.module_eval do
-        puts 'Test 2'
-        alias_method :original_start, :start
-        def start(options={})
-          puts 'CUSTOM COMMANDER STARTING UP!'
-          original_start
-        end
-      end
+    extend OriginalGuardCommander
+    def start(options={})
+      puts 'CUSTOM COMMANDER STARTING UP!'
+      # OriginalGuardCommander
     end
+    # def self.included(base)
+    #   puts 'Test 1'
+    #   base.module_eval do
+    #     puts 'Test 2'
+    #     alias_method :original_start, :start
+    #     def start(options={})
+    #       puts 'CUSTOM COMMANDER STARTING UP!'
+    #       original_start
+    #     end
+    #   end
+    # end
   end
   extend Commander
 end
