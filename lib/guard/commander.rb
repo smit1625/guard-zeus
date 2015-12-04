6f0fb3f17923d1aebaf4027db83c58c265ad0d95
MacKinley Smith <smit1625@msu.edu>
2015-12-04 15:10:57 -0700

    Maybe we can use `module_eval`?

--- lib/guard/commander.rb
+++ lib/guard/commander.rb
@@ -10,7 +10,7 @@ require "guard/internals/state"
 module Guard
   module Commander
     def self.included(base)
-      base.class_eval do
+      base.module_eval do
         alias_method :original_start, :start
         def start(options={})
           puts 'CUSTOM COMMANDER STARTING UP!'
